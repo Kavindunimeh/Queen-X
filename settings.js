62d1280202cde2d50ccaffc76cc374f3002a7b82
Kavindunimeh <104300472+Kavindunimeh@users.noreply.github.com>
2023-02-04 19:43:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94719225807'] //ur owner number
-global.ownername = "©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ" //ur owner name
+global.owner = ['94763204765'] //ur owner number
+global.ownername = "۝⃟̥̩̩̩̥̩̥͚̮𝘒𝘢𝘷𝘪𝘯𝘥𝘶۝⃟̥̩̩̩̥̩̥͚̮💍𝘴𝘵𝘢𝘳1😮⃝⃟۝᭄̊̊࿔࿓" //ur owner name
 global.ytname = "ʏᴜʀᴇꜱʜ" //ur yt chanel name
-global.socialm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur github or insta name
+global.socialm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝚋𝚢 😮⃝৫⃟➤ƙαʋιɳԃυ᭄᭄̊̊̊̊ ʂƚαɾ1࿓࿔🏻" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.ownernomer = "94719225807" //ur number
-global.premium = ['94719225807'] //ur premium number
-global.botname = '©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ' //ur bot name
+global.ownernomer = "94763204765" //ur number
+global.premium = ['94763204765'] //ur premium number
+global.botname = '©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝚋𝚢 😮⃝৫⃟➤ƙαʋιɳԃυ᭄᭄̊̊̊̊ ʂƚαɾ1࿓࿔ ' //ur bot name
 global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
 global.websitex = "https:// no" //ur website to be displayed
 global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
 global.themeemoji = "🛸" //ur theme emoji
-global.packname = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur sticker watermark packname
-global.author = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻\n\n\nA whatsapp bot developed by\nCYBORG ROBOT" //ur sticker watermark author
-global.wm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻." //ur watermark
+global.packname = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝚋𝚢 😮⃝৫⃟➤ƙαʋιɳԃυ᭄᭄̊̊̊̊ ʂƚαɾ1࿓࿔ " //ur sticker watermark packname
+global.author = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻\n\n\nA whatsapp bot developed by\nCYBORG ROBOT 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝚋𝚢 😮⃝৫⃟➤ƙαʋιɳԃυ᭄᭄̊̊̊̊ ʂƚαɾ1࿓࿔ " //ur sticker watermark author
+global.wm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻.  𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝚋𝚢 😮⃝৫⃟➤ƙαʋιɳԃυ᭄᭄̊̊̊̊ ʂƚαɾ1࿓࿔" //ur watermark
 
 // Other
 global.sessionName = 'session'
